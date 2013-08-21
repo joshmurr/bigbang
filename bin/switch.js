65fade24aa8649fb1dc3f16f95bbc0432b8c52ee
Josh Murr <joshuadavidmurr@gmail.com>
2013-08-22 00:15:10 +0100

    About to make some biggish changes. Currently stable...

---
+++ bin/switch.js
@@ -0,0 +1,8 @@
+switch(state){
+	case 0:
+		systems.unshift(new particleSystem(new vec(****,***), +ps.numParts, ps.keepEmitting));
+		systems[f].attractors.push(new attractor(new vec(+att.x, +att.y), +att.mass, +att.G));
+		//PARTICLE COLOUR
+		//STOP EMITTING
+		//ATTRACT ALL
+}
