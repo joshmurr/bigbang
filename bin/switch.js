77362ccdc47d2073d42d9eabdb9a89204fdf8210
Josh Murr <joshuadavidmurr@gmail.com>
2013-08-26 02:48:36 +0100

    Remove bin

--- bin/switch.js
+++
@@ -1,8 +0,0 @@
-switch(state){
-	case 0:
-		systems.unshift(new particleSystem(new vec(****,***), +ps.numParts, ps.keepEmitting));
-		systems[f].attractors.push(new attractor(new vec(+att.x, +att.y), +att.mass, +att.G));
-		//PARTICLE COLOUR
-		//STOP EMITTING
-		//ATTRACT ALL
-}
